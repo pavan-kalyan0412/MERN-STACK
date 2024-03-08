f3bdaa2559b52dde8dd27bfb2ab066be5bea7560
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-03-08 11:25:28 +0530

    added record files route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/record", records);
 
 app.get('/',(req,res)=>{
     res.send("<h1 style='display:inline;margin:0;padding:0;'>hey,</h1><h3 style='display:inline;margin:0;padding:15px;'>mern developer</h3>");
